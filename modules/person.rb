5f0e626220aa3f0077ce46ba49527f5e800d7187
ikoote1 <ikootepreim@gmail.com>
2023-05-15 16:52:36 +0300

    working on

--- modules/person.rb
+++ modules/person.rb
@@ -36,5 +36,12 @@ class Person
 end
 
 class Student < Person
+    def initialize
+        super(classroom)
+        @classroom = classroom
+    end
 
+    def play_hooky
+        "¯\(ツ)/¯"
+    end
 end
