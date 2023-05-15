c2c9b1ed05e8891be6161fad59a6a27b7b770821
ikoote1 <ikootepreim@gmail.com>
2023-05-15 17:39:12 +0300

    code fix

--- modules/person.rb
+++ modules/person.rb
@@ -1,34 +1,33 @@
 class Person
-    def initialize (name = 'Unknown',age,parent_permission = true)
-        @id = Random.rand(1..10000)
-        @name = name
-        @age = age
-        @parent_permission = parent_permission
-    end
-    def id
-        @id
-    end
-    def name
-        @name
-    end
-    def age
-        @age
-    end
+  def initialize(age, name = 'Unknown')
+    @id = Random.rand(1..10_000)
+    @name = name
+    @age = age
+    #   @parent_permission = parent_permission
+  end
+  # def id
+  #     @id
+  # end
+  # def name
+  #     @name
+  # end
+  # def age
+  #     @age
+  # end
 
-    def name=(new_name)
-        @name = new_name
-    end
+  # def name=(new_name)
+  #     @name = new_name
+  # end
 
-    def age=(new_age)
-        @age = new_age
-    end
-    private
-    def of_age?
-        @age >= 18
-    end
-    public
-    def can_use_services?
-        of_age? || parent_permission
-    end
+  # def age=(new_age)
+  #     @age = new_age
+  # end
+  # private
+  # def of_age?
+  #     @age >= 18
+  # end
+  # public
+  # def can_use_services?
+  #     of_age? || parent_permission
+  # end
 end
-
