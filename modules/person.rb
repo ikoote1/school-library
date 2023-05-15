e37d5c628d04720ec32ebe12a668b3466c5b22a1
ikoote1 <ikootepreim@gmail.com>
2023-05-15 16:16:19 +0300

    working on

--- modules/person.rb
+++ modules/person.rb
@@ -1,2 +1,16 @@
 class Person
+    def initialize (name = 'Unknown',age,parent_permission = true)
+        @id 
+        @name = name
+        @age = age
+    end
+    def id
+        @id
+    end
+    def name
+        @name
+    end
+    def age
+        @age
+    end
 end
