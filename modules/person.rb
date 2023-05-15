8a3f03651df88b213b12d51faae8adc41ab21af4
ikoote1 <ikootepreim@gmail.com>
2023-05-15 17:42:12 +0300

    code fix

--- modules/person.rb
+++ modules/person.rb
@@ -3,7 +3,7 @@ class Person
     @id = Random.rand(1..10_000)
     @name = name
     @age = age
-    #   @parent_permission = parent_permission
+    @parent_permission = true
   end
 
   attr_accessor :name, :age
