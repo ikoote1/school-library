c56395253147aff0e4348ecb3a7c343cec626ebf
ikoote1 <ikootepreim@gmail.com>
2023-05-15 17:41:09 +0300

    code fix

--- modules/person.rb
+++ modules/person.rb
@@ -5,29 +5,19 @@ class Person
     @age = age
     #   @parent_permission = parent_permission
   end
-  # def id
-  #     @id
-  # end
-  # def name
-  #     @name
-  # end
-  # def age
-  #     @age
-  # end
 
-  # def name=(new_name)
-  #     @name = new_name
-  # end
+  attr_accessor :name, :age
+  attr_reader :id
 
-  # def age=(new_age)
-  #     @age = new_age
-  # end
-  # private
-  # def of_age?
-  #     @age >= 18
-  # end
-  # public
-  # def can_use_services?
-  #     of_age? || parent_permission
-  # end
+  private
+
+  def of_age?
+    @age >= 18
+  end
+
+  public
+
+  def can_use_services?
+    of_age? || parent_permission
+  end
 end
