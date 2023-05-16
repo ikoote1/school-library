11507ff1ff21266a61f732a553ba96d6f93b2afb
ikoote1 <ikootepreim@gmail.com>
2023-05-16 15:46:58 +0300

    decorating

--- modules/nameable.rb
+++ modules/nameable.rb
@@ -23,6 +23,7 @@ class CapitalizeDecorator < BaseDecorator
 end
 class TrimmerDecorator < BaseDecorator
   def correct_name
-	@nameeable.correct_name
+    trimmed_name = @nameable.correct_name[0..9]
+    trimmed_name.length < @nameable.correct_name.length ? trimmed_name : @nameable.correct_name
   end
 end
