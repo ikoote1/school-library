eb0e7f90190148df029e57fd45742a963ee1eb3a
ikoote1 <ikootepreim@gmail.com>
2023-05-16 15:48:58 +0300

    decorating

--- modules/person.rb
+++ modules/person.rb
@@ -27,3 +27,11 @@ require "./nameable.rb"
     of_age? || parent_permission
   end
 end
+
+
+person = Person.new(22, 'maximilianus')
+  person.correct_name
+  capitalizedPerson = CapitalizeDecorator.new(person)
+  capitalizedPerson.correct_name
+  capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
+  capitalizedTrimmedPerson.correct_name
