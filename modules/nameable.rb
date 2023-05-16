fd68331c54571ccfe390dd6a893572adfb0d7caf
ikoote1 <ikootepreim@gmail.com>
2023-05-16 15:49:42 +0300

    decorating

--- modules/nameable.rb
+++ modules/nameable.rb
@@ -4,26 +4,4 @@ class Nameable
   end
 end
 
-class BaseDecorator < Nameable
-  attr_accessor :nameable
 
-  def initialize(nameable)
-	@nameable = nameable
-  end
-
-  def correct_name
-	@nameable.correct_name
-  end
-end
-
-class CapitalizeDecorator < BaseDecorator
-  def correct_name
-	@nameable.correct_name.capitalize
-  end
-end
-class TrimmerDecorator < BaseDecorator
-  def correct_name
-    trimmed_name = @nameable.correct_name[0..9]
-    trimmed_name.length < @nameable.correct_name.length ? trimmed_name : @nameable.correct_name
-  end
-end
--- modules/person.rb
+++ modules/person.rb
@@ -28,6 +28,30 @@ require "./nameable.rb"
   end
 end
 
+class BaseDecorator < Nameable
+  attr_accessor :nameable
+
+  def initialize(nameable)
+	@nameable = nameable
+  end
+
+  def correct_name
+	@nameable.correct_name
+  end
+end
+
+class CapitalizeDecorator < BaseDecorator
+  def correct_name
+	@nameable.correct_name.capitalize
+  end
+end
+class TrimmerDecorator < BaseDecorator
+  def correct_name
+    trimmed_name = @nameable.correct_name[0..9]
+    trimmed_name.length < @nameable.correct_name.length ? trimmed_name : @nameable.correct_name
+  end
+end
+
 
 person = Person.new(22, 'maximilianus')
   person.correct_name
