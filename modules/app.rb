b021800a1179ab4e9a19e024cbb89f9f866fc10b
ikoote1 <ikootepreim@gmail.com>
2023-05-19 10:19:26 +0300

    console-app

--- modules/app.rb
+++ modules/app.rb
@@ -17,150 +17,150 @@ class App
   def selected_option(option)
     case option
 
-when '1'
-  list_books
-when '2'
-  list_people
-when '3'
-  create_person
-when '4'
-  create_book
-when '5'
-  create_rental
-when '6'
-  list_rentals
-else
-  'Please select valid option from the list!'
-end
-end
-
-def list_books
-@books.each do |book|
-  book.instance_variables.each do |var|
-    text = ''
-    value = book.instance_variable_get(var)
-    var = var.to_s.delete('@')
-    var = var.capitalize
-    text += "#{var}:#{value}" unless var.include?('Rentals')
-    puts text
+    when '1'
+      list_books
+    when '2'
+      list_people
+    when '3'
+      create_person
+    when '4'
+      create_book
+    when '5'
+      create_rental
+    when '6'
+      list_rentals
+    else
+      'Please select valid option from the list!'
+    end
   end
-end
-end
 
-def list_people
-@people.each do |person|
-  if person.is_a?(Student)
-    puts "[Student] Name: #{person.name}, ID: #{person.object_id}, Age: #{person.age}"
-  elsif person.is_a?(Teacher)
-    puts "[Teacher] Name: #{person.name}, ID: #{person.object_id}, Age: #{person.age}"
-  else
-    puts "Name: #{person.name}"
+  def list_books
+    @books.each do |book|
+      book.instance_variables.each do |var|
+        text = ''
+        value = book.instance_variable_get(var)
+        var = var.to_s.delete('@')
+        var = var.capitalize
+        text += "#{var}:#{value}" unless var.include?('Rentals')
+        puts text
+      end
+    end
   end
-  puts "\n"
-end
-end
 
-def create_person
-puts 'Do you want to create a student(1) or a teacher(2)?'
-choise = gets.chomp
+  def list_people
+    @people.each do |person|
+      if person.is_a?(Student)
+        puts "[Student] Name: #{person.name}, ID: #{person.object_id}, Age: #{person.age}"
+      elsif person.is_a?(Teacher)
+        puts "[Teacher] Name: #{person.name}, ID: #{person.object_id}, Age: #{person.age}"
+      else
+        puts "Name: #{person.name}"
+      end
+      puts "\n"
+    end
+  end
 
-if choise == '1'
-  create_student
-elsif choise == '2'
-  create_teacher
-else
-  puts 'Your input is wrong!'
-end
-end
+  def create_person
+    puts 'Do you want to create a student(1) or a teacher(2)?'
+    choise = gets.chomp
 
-def create_student
-puts 'Age:'
-age = gets.chomp
-puts 'Name:'
-name = gets.chomp
-puts 'Has parent permission? [Y/N]'
-parent_permission = gets.chomp.upcase == 'Y'
-student = Student.new(age, name, parent_permission)
-@people.push(student)
-puts 'You added student successfully'
-end
+    if choise == '1'
+      create_student
+    elsif choise == '2'
+      create_teacher
+    else
+      puts 'Your input is wrong!'
+    end
+  end
 
-def create_teacher
-puts 'Age:'
-age = gets.chomp
-puts 'Name:'
-name = gets.chomp
-puts 'Specialization:'
-specialization = gets.chomp
-teacher = Teacher.new(age, name, specialization)
-@people.push(teacher)
-puts 'You added teacher successfully'
-end
+  def create_student
+    puts 'Age:'
+    age = gets.chomp
+    puts 'Name:'
+    name = gets.chomp
+    puts 'Has parent permission? [Y/N]'
+    parent_permission = gets.chomp.upcase == 'Y'
+    student = Student.new(age, name, parent_permission)
+    @people.push(student)
+    puts 'You added student successfully'
+  end
 
-def create_book
-puts 'Title:'
-title = gets.chomp
-puts 'Author:'
-author = gets.chomp
-book = Book.new(title, author)
-@books.push(book)
-puts 'Book created successfully'
-end
+  def create_teacher
+    puts 'Age:'
+    age = gets.chomp
+    puts 'Name:'
+    name = gets.chomp
+    puts 'Specialization:'
+    specialization = gets.chomp
+    teacher = Teacher.new(age, name, specialization)
+    @people.push(teacher)
+    puts 'You added teacher successfully'
+  end
 
-def books_with_index
-@books.each_with_index do |book, index|
-  text = "#{index}: "
-  book.instance_variables.each do |var|
-    val = book.instance_variable_get(var)
-    var = var.to_s.delete('@')
-    text += "#{var}:#{val} " unless var.include?('rentals') || var.include?('classroom')
-end
-puts text
-end
-end
+  def create_book
+    puts 'Title:'
+    title = gets.chomp
+    puts 'Author:'
+    author = gets.chomp
+    book = Book.new(title, author)
+    @books.push(book)
+    puts 'Book created successfully'
+  end
 
-def people_with_index
-@people.each_with_index do |person, index|
-text = "#{index}: "
-person.instance_variables.each do |var|
-  val = person.instance_variable_get(var)
-  var = var.to_s.delete('@')
-  text += "#{var}:#{val} " unless var.include?('rentals') || var.include?('classroom')
-end
-puts text
-end
-end
+  def books_with_index
+    @books.each_with_index do |book, index|
+      text = "#{index}: "
+      book.instance_variables.each do |var|
+        val = book.instance_variable_get(var)
+        var = var.to_s.delete('@')
+        text += "#{var}:#{val} " unless var.include?('rentals') || var.include?('classroom')
+      end
+      puts text
+    end
+  end
 
-def create_rental
-puts 'Select a book from the following list by number'
-books_with_index
-selected_book = gets.chomp.to_i
-puts 'Select a person from the following list by number (not ID)'
-people_with_index
-selected_person = gets.chomp.to_i
-print 'Date: '
-date = gets.chomp
-book = @books[selected_book]
-person = @people[selected_person]
-rental = Rental.new(date, book, person)
-@rentals.push(rental)
-puts 'Rental created successfully'
-end
+  def people_with_index
+    @people.each_with_index do |person, index|
+      text = "#{index}: "
+      person.instance_variables.each do |var|
+        val = person.instance_variable_get(var)
+        var = var.to_s.delete('@')
+        text += "#{var}:#{val} " unless var.include?('rentals') || var.include?('classroom')
+      end
+      puts text
+    end
+  end
 
-def list_rentals
-print 'To see person rentals enter the person ID: '
-id = gets.chomp.to_i
-puts 'Rented Books:'
-@rentals.each do |rental|
-person = rental.instance_variable_get(:@person)
-person_id = person.instance_variable_get(:@id)
+  def create_rental
+    puts 'Select a book from the following list by number'
+    books_with_index
+    selected_book = gets.chomp.to_i
+    puts 'Select a person from the following list by number (not ID)'
+    people_with_index
+    selected_person = gets.chomp.to_i
+    print 'Date: '
+    date = gets.chomp
+    book = @books[selected_book]
+    person = @people[selected_person]
+    rental = Rental.new(date, book, person)
+    @rentals.push(rental)
+    puts 'Rental created successfully'
+  end
 
-next unless person_id == id
+  def list_rentals
+    print 'To see person rentals enter the person ID: '
+    id = gets.chomp.to_i
+    puts 'Rented Books:'
+    @rentals.each do |rental|
+      person = rental.instance_variable_get(:@person)
+      person_id = person.instance_variable_get(:@id)
 
-book = rental.instance_variable_get(:@book)
-title = book.instance_variable_get(:@title)
-author = book.instance_variable_get(:@author)
-puts "Date: #{rental.date} Book: #{title} by Author: #{author} "
-end
-end
+      next unless person_id == id
+
+      book = rental.instance_variable_get(:@book)
+      title = book.instance_variable_get(:@title)
+      author = book.instance_variable_get(:@author)
+      puts "Date: #{rental.date} Book: #{title} by Author: #{author} "
+    end
+  end
 end
