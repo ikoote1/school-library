7bf83d7eb46448437f117eade02c3fb7b2a2a643
ikoote1 <ikootepreim@gmail.com>
2023-05-29 15:45:33 +0300

    main-solid

--- modules/runing.rb
+++ modules/runing.rb
@@ -8,4 +8,4 @@ puts person.correct_name
 capitalized_person = CapitalizeDecorator.new(person)
 puts capitalized_person.correct_name
 capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
+# puts capitalized_trimmed_person.correct_name
